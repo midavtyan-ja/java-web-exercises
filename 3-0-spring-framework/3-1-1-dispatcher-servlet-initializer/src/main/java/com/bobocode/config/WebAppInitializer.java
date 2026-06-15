5efad2deb0bc75357db6626f4888d9e7d428b7ef
midavtyan-ja <midavtyan@griddynamics.com>
2026-06-15 23:03:34 +0400

    Task The task is to configure a typical Spring MVC application separating web and root configs. Your job is to follow the instructions in the todo section and implement a proper configuration.
    To verify your configuration, run WelcomeWebAppTest.java

--- 3-0-spring-framework/3-1-1-dispatcher-servlet-initializer/src/main/java/com/bobocode/config/WebAppInitializer.java
+++ 3-0-spring-framework/3-1-1-dispatcher-servlet-initializer/src/main/java/com/bobocode/config/WebAppInitializer.java
@@ -1,24 +1,21 @@
 package com.bobocode.config;
 
-import com.bobocode.util.ExerciseNotCompletedException;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-/**
- * This class is used to configure DispatcherServlet and links it with application config classes
- */
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        throw new ExerciseNotCompletedException(); //todo: use {@link RootConfig} as root application config class
+        return new Class[]{RootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        throw new ExerciseNotCompletedException(); //todo: use {@link WebConfig} as ServletConfig class
+        return new Class[]{WebConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        throw new ExerciseNotCompletedException(); //todo: provide default servlet mapping ("/")
+        return new String[]{"/"};
     }
 }
